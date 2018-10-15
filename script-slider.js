70dfb380bcfea5a3ccfcafda9c664af20c4f1c69
ilya <haabwork@bk.ru>
2018-10-15 18:23:22 +0500

    first

---
+++ script-slider.js
@@ -0,0 +1,113 @@
+$(function () {
+	inputResult = $("#result");
+
+
+	function getSlider(str, num, jpgPng, start, end, dataSize1, dataSize2, mTop, mBottom, image80, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly) {
+
+		var i = +start || 1;
+		var max = Math.max.apply(null, arrOnly);
+		if (end) {
+			num = end;
+		} else if (max) {
+			num = max;
+		}
+
+
+		mTop = mTop || "2";
+		mBottom = mBottom || "2";
+
+		addCodeTop = addCodeTop || "";
+		addCodeBottom = addCodeBottom || "";
+
+		dataSize1 = dataSize1 || "1200";
+		dataSize2 = dataSize2 || "800";
+
+		var x;
+		if (dataSize1) {
+			x = "x";
+		} else {
+			x = "";
+		}
+
+
+		var startImage80, endImage80;
+		if (image80) {
+			startImage80 = '\n<div class="image80">';
+			endImage80 = '</div>\n';
+		} else {
+			startImage80 = '';
+			endImage80 = '';
+		}
+		if (myClass) {
+			startImage80 = '\n<div class="' + myClass + '">';
+			endImage80 = '</div>\n';
+		}
+
+		jpgPng ? jpgPng = 'png' : jpgPng = 'jpg';
+
+		var passCheck;
+		var onlyCheck;
+
+		for (i; i <= num; i++) {
+			function isPositive(number) {
+				return number == i; //Проверяем массив на совпадение
+			}
+
+			function isNegative(number2) {
+				return number2 == i; //Проверяем массив на совпадение
+			}
+			passCheck = arrPass.some(isPositive);
+			if (passCheck) {
+				continue;
+			}
+			if (arrOnly[1]) {
+				onlyCheck = arrOnly.some(isNegative);
+				if (!onlyCheck) {
+					continue;
+				}
+			}
+
+			if (i < 10) {
+				i = "0" + i;
+			}
+			str = str + '\n<figure itemprop="associatedMedia">\n<a href="' + i + '.' + jpgPng + '" itemprop="contentUrl" data-size="' + dataSize1 + x + dataSize2 + '">\n<img src="' + i + '.' + jpgPng + '" itemprop="thumbnail" class="item " />\n</a>\n<figcaption itemprop="caption description">\n \n</figcaption>\n<pswp__caption__center itemprop="caption description">\n<div class="caption_padding">\n \n</div>\n</pswp__caption__center>\n</figure>\n';
+		}
+
+		return addCodeTop + startImage80 + '\n<div class="zooming sliding_gallery" itemscope style="margin:auto; margin-top: ' + mTop + 'em; margin-bottom: ' + mBottom + 'em;">\n' + str + '\n</div>\n' + endImage80 + addCodeBottom;
+	}
+
+
+	$("#btnSlide").on("click", function () {
+		var inputNum = $("#text");
+		var jpgPng = $("#jpg-png-slider").prop('checked');
+		var start = $("#start").val();
+		var end = $("#end").val();
+		var dataSize1 = $("#dataSize1-slider").val();
+		var dataSize2 = $("#dataSize2-slider").val();
+		var mTop = $("#mTop-slider").val();
+		var mBottom = $("#mBottom-slider").val();
+		var myClass = $("#my-class-slider").val();
+		var addCodeTop = $("#addcode-top-slider").val();
+		var addCodeBottom = $("#addcode-bottom-slider").val();
+		var pass = $("#pass").val();
+		var only = $("#only").val();
+		var image80 = $("#image80-slider").prop('checked');
+
+		var num = +inputNum.val();
+		var arrPass = pass.split(' ');
+		var arrOnly = only.split(' ');
+
+		var str = '';
+		str = getSlider(str, num, jpgPng, start, end, dataSize1, dataSize2, mTop, mBottom, image80, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly);
+		inputResult.val(str);
+
+		//Автоматическое копирование в буфер обменм
+		event.preventDefault();
+		inputResult.select();
+		document.execCommand("copy");
+		//Автоматическое копирование в буфер обменм end
+
+		//console.log(jpgPng); .replace(/ /g, '')
+	})
+
+});
---
+++ script.js
@@ -0,0 +1,251 @@
+$(function () {
+
+	inputResult = $("#result");
+
+	//Для удобства
+	$('#text').focus(); //При загрузки ставим фокус в "текст"
+
+	$('#text').focus(function () {
+		inputResult.val('');
+		inputText.val('');
+	});
+
+	//Для удобства end
+
+	function getMin(str, num, jpgPng, border, start, end, dataSize1, dataSize2, mTop, mBottom, image80, shadow, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly) {
+		var i = +start || "1";
+		var max = Math.max.apply(null, arrOnly);
+
+		if (end) {
+			num = end;
+		} else if (max) {
+			num = max;
+		}
+
+		addCodeTop = addCodeTop || "";
+		addCodeBottom = addCodeBottom || "";
+
+
+		mTop = mTop || "2";
+		mBottom = mBottom || "2";
+
+		var x;
+		if (dataSize1) {
+			x = "x";
+		} else {
+			x = "";
+		}
+
+		var startImage80, endImage80;
+		if (image80) {
+			startImage80 = '\n<div class="image80">';
+			endImage80 = '</div>\n';
+		} else {
+			startImage80 = '';
+			endImage80 = '';
+		}
+		if (myClass) {
+			startImage80 = '\n<div class="' + myClass + '">';
+			endImage80 = '</div>\n';
+		}
+
+		shadow ? shadow = 'shadow' : shadow = '';
+
+		jpgPng ? jpgPng = 'png' : jpgPng = 'jpg';
+		border ? border = 'frame' : border = '';
+
+		/* function isSome(number) {
+			return number === i;
+		} */
+		var passCheck;
+		var onlyCheck;
+
+		for (i; i <= num; i++) {
+			function isPositive(number) {
+				return number == i; //Проверяем массив на совпадение
+			}
+
+			function isNegative(number2) {
+				return number2 == i; //Проверяем массив на совпадение
+			}
+			passCheck = arrPass.some(isPositive);
+			if (passCheck) {
+				continue;
+			}
+			if (arrOnly[1]) {
+				onlyCheck = arrOnly.some(isNegative);
+				if (!onlyCheck) {
+					continue;
+				}
+			}
+
+			if (i < 10) {
+				i = "0" + i;
+			}
+
+			str = str + addCodeTop + startImage80 + '\n<div class="zooming">\n<figure itemprop="associatedMedia" style="margin-top: ' + mTop + 'em; margin-bottom: ' + mBottom + 'em">\n<a href="' + i + '.' + jpgPng + '" itemprop="contentUrl" data-size="' + dataSize1 + x + dataSize2 + '">\n<img src="' + i + '.' + jpgPng + '" itemprop="thumbnail" class="' + border + shadow + '" />\n</a>\n</figure>\n</div>\n' + endImage80 + addCodeBottom;
+		}
+		return str;
+	}
+
+	function getMax(str, num, jpgPng, border, start, end, dataSize1, dataSize2, mTop, mBottom, image80, shadow, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly) {
+		var i = +start || 1;
+		var max = Math.max.apply(null, arrOnly);
+			if (end) {
+				num = end;
+			} else if (max) {
+				num = max;
+			}
+
+
+
+		addCodeTop = addCodeTop || "";
+		addCodeBottom = addCodeBottom || "";
+
+
+		mTop = mTop || "2";
+		mBottom = mBottom || "1";
+
+		var x;
+		if (dataSize1) {
+			x = "x";
+		} else {
+			x = "";
+		}
+
+
+		var startImage80, endImage80;
+		if (image80) {
+			startImage80 = '\n<div class="image80">';
+			endImage80 = '</div>\n';
+		} else {
+			startImage80 = '';
+			endImage80 = '';
+		}
+		if (myClass) {
+			startImage80 = '\n<div class="' + myClass + '">';
+			endImage80 = '</div>\n';
+		}
+
+		shadow ? shadow = 'shadow' : shadow = '';
+
+		jpgPng ? jpgPng = 'png' : jpgPng = 'jpg';
+		border ? border = 'frame' : border = '';
+
+		var passCheck;
+		var onlyCheck;
+
+		for (i; i <= num; i++) {
+			function isPositive(number) {
+				return number == i; 
+			}
+
+			function isNegative(number2) {
+				return number2 == i; 
+			}
+			passCheck = arrPass.some(isPositive);
+			if (passCheck) {
+				continue;
+			}
+			if (arrOnly[1]) {
+				onlyCheck = arrOnly.some(isNegative);
+				if (!onlyCheck) {
+					continue;
+				}
+			}
+
+			if (i < 10) {
+				i = "0" + i;
+			}
+			str = str + addCodeTop + startImage80 + '\n<div class="zooming">\n<figure itemprop="associatedMedia" style="margin-top: ' + mTop + 'em; margin-bottom: ' + mBottom + 'em">\n<a href="' + i + '.' + jpgPng + '" itemprop="contentUrl" data-size="' + dataSize1 + x + dataSize2 + '">\n<img src="' + i + '.' + jpgPng + '" itemprop="thumbnail" class="' + border + ' ' + shadow + '" />\n</a>\n<figcaption itemprop="caption description">\n </figcaption>\n</figure>\n</div>\n' + endImage80 + '<figcaption itemprop="caption description">\n</figcaption>\n' + addCodeBottom;
+		}
+		return str;
+	}
+
+	$("#btn").on("click", function () {
+		var inputNum = $("#text");
+		var jpgPng = $("#jpg-png").prop('checked');
+		var border = $("#border").prop('checked');
+		var shadow = $("#shadow").prop('checked');
+		var start = $("#start").val();
+		var end = $("#end").val();
+		var dataSize1 = $("#dataSize1").val();
+		var dataSize2 = $("#dataSize2").val();
+		var mTop = $("#mTop").val();
+		var mBottom = $("#mBottom").val();
+		var myClass = $("#my-class").val();
+		var pass = $("#pass").val();
+		var only = $("#only").val();
+		var addCodeTop = $("#addcode-top").val();
+		var addCodeBottom = $("#addcode-bottom").val();
+		var image80 = $("#image80").prop('checked');
+
+		var str = '';
+		var num = +inputNum.val();
+
+		var arrPass = pass.split(' ');
+		var arrOnly = only.split(' ');
+
+		if ($('#min-max').prop('checked')) {
+			str = getMax(str, num, jpgPng, border, start, end, dataSize1, dataSize2, mTop, mBottom, image80, shadow, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly);
+		} else {
+			str = getMin(str, num, jpgPng, border, start, end, dataSize1, dataSize2, mTop, mBottom, image80, shadow, myClass, addCodeTop, addCodeBottom, arrPass, arrOnly);
+		}
+		inputResult.val(str);
+
+		//Автоматическое копирование в буфер обменм
+		event.preventDefault();
+		inputResult.select();
+		document.execCommand("copy");
+		//Автоматическое копирование в буфер обменм end
+
+		//console.log(jpgPng); .replace(/ /g, '')
+	});
+
+
+	$("#btnSlide").on("click", function () {
+
+
+	});
+
+
+
+
+
+
+
+
+	$("#min-max").change(function () {
+		if (this.checked) {
+			$("#mBottom").attr("placeholder", "1");
+		} else {
+			$("#mBottom").attr("placeholder", "2");
+		}
+	});
+	$(".reset").on("click", function () {
+		$("#start, #end, #text").val("");
+	});
+
+	var flagSlider = 0;
+	$("#slider").on("click", function () {
+		if (flagSlider === 0) {
+			$("#slider h4").text("Zoom");
+			$("#btn").toggleClass("d-n");
+			$("#btnSlide").toggleClass("d-n");
+			flagSlider = 1;
+		} else {
+			$("#slider h4").text("Слайдер");
+			$("#btn").toggleClass("d-n");
+			$("#btnSlide").toggleClass("d-n");
+			flagSlider = 0;
+		}
+		$(".slider__container").toggleClass("d-n");
+		$(".common__container").toggleClass("d-n");
+
+
+	});
+
+
+
+
+});
